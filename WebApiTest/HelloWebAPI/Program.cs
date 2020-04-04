e75312ca4ce6615de5a377aac33099a37aea33a2
jswjia@163.com <jswjia@163.com>
2020-04-04 21:44:12 +0800

    添加接口demo

---
+++ WebApiTest/HelloWebAPI/Program.cs
@@ -0,0 +1,31 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+
+namespace HelloWebAPI
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            CreateHostBuilder(args).Build().Run();
+        }
+
+        public static IHostBuilder CreateHostBuilder(string[] args) {
+            var hostConfig = new ConfigurationBuilder().AddJsonFile("appsettings.json").Build();
+
+            return Host.CreateDefaultBuilder(args)
+                   .ConfigureWebHostDefaults(webBuilder =>
+                   {
+                       webBuilder.UseUrls(hostConfig["ServiceHost"]);
+                       webBuilder.UseStartup<Startup>();
+                   });
+        }
+           
+    }
+}
